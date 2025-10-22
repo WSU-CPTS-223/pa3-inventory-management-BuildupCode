78c1335ef7f489e90b66076c99c89daf1cdd74cb
youngukkim <young-uk.kim@wsu.edu>
2025-10-21 21:20:58 -0700

    Add some comment in product.h and product.cpp.

--- product.cpp
+++ product.cpp
@@ -1,28 +1,31 @@
 #include "product.h"
 #include <iostream>
-
+//constructor
 Product::Product() {}
 
 Product::Product(const std::string& id, const std::string& name, const std::string& cat_raw)
 : uniq_id(id), product_name(name), category_string_raw(cat_raw){}
-
+//return id
 std::string Product::getId() const{
     return uniq_id;
 }
+//return name
 std::string Product::getName() const{
     return product_name;
 }
+//return original category 
 std::string Product::getCategoryRaw() const{
     return category_string_raw;
 }
+//return parsed category list
 const Vector<std::string>& Product::getCategories() const{
     return categories;
 }
-
+//set parsed category list
 void Product::setCategories(const Vector<std::string>& cats){
     categories = cats;
 }
-
+//printing informations
 void Product::printDetails() const{
     std::cout<<"ID: "<<uniq_id<<"\nName: "<<product_name<<"\nCategories: "<<category_string_raw<<std::endl;
 }
--- product.h
+++ product.h
@@ -1,23 +1,32 @@
 #ifndef PRODUCT_H
 #define PRODUCT_H
-
+//for using std::string
 #include <string>
+//my own vector.h
 #include "vector.h"
 
 class Product{
 private:
     std::string uniq_id;
     std::string product_name;
+    //category list from csv
     std::string category_string_raw;
+    //return parsed category 
     Vector<std::string> categories;
 public:
     Product();
     Product(const std::string& id, const std::string& name, const std::string& cat_raw);
+    //return id
     std::string getId() const;
+    //return name
     std::string getName() const;
+    //return original category from csv
     std::string getCategoryRaw() const;
+    //return parsed category list
     const Vector<std::string>& getCategories() const;
+    //set parsed category list
     void setCategories(const Vector<std::string>& cats);
+    //printing informations
     void printDetails() const;
 
 };

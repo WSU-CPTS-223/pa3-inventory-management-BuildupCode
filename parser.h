b78811b913e2132360e4cacd050c4486c067178b
youngukkim <young-uk.kim@wsu.edu>
2025-10-22 23:26:45 -0700

    Prepare for parser.h

---
+++ parser.h
@@ -0,0 +1,22 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <string>
+#include "vector.h"
+#include "product.h"
+
+class Parser{
+public:
+    struct ParsedRecird{
+        std::string uniq_id;
+        std::string product_name;
+        std::string category_raw;
+    };
+
+    //static
+
+private:
+
+};
+
+#endif

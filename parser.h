c562cf599f152772257d3b5ff58c2a6551297607
youngukkim <young-uk.kim@wsu.edu>
2025-10-23 00:51:19 -0700

    Create code for parser.h. Ckech comment here later after professor's answer.

--- parser.h
+++ parser.h
@@ -7,15 +7,30 @@
 
 class Parser{
 public:
+    //change comment later here
+    //read from csv(expected)
     struct ParsedRecird{
         std::string uniq_id;
         std::string product_name;
         std::string category_raw;
     };
-
-    //static
+    //parse 1 line from csv
+    //if success, return true.
+    static bool  parseCsvLine(const std::string& line, ParsedRecird& output);
+    //divide category  string by '|'. If raw is empty, fill 'NA'
+    static Vector<std::string> parseCategories(const std::string& category_new);
+    //make product from parsed record
+    static Product makeProduct(const ParsedRecird& record);
+    //remove whitespace from string
+    static std::string trim(const std::string& string);
+    //remove quote from string
+    static std::string removeQuote(const std::string& string);
 
 private:
+    //extract next csv field starting at position
+    static std::string readCsvField(const std::string& line, std::size_t& position);
+    //check whitespace is space, tab, CR or LF
+    static bool checkWhitespace(char w);
 
 };
 
